2aaa61047f05f05cca1e4d1738f3b46a09c8ce14
chengtong <xyjyct@163.com>
2016-08-30 15:47:28 +0800

    improve

--- designer/src/main/webapp/resources/js/app/dataAnalysis.js
+++ designer/src/main/webapp/resources/js/app/dataAnalysis.js
@@ -266,6 +266,25 @@ require(['jquery','ztree','jqueryCookie','jqueryMd5','bootstrap'], function($,zt
                             drop: function(event,ui){
                                 var target = $(this);
                                 tagDropFunction(event,ui,'fa fa-tachometer',target)
+                                
+                                $.ajax({
+                                    type: 'POST',
+                                    contentType: "application/json; charset=utf-8",
+                                    url: '../render',
+                                    data: JSON.stringify({
+                                        'chartType': chartType,
+                                        'dataRecordId': sqlRecordingId,
+                                        'exportId': window.location.href.split("=")[1].replace("&order",""),
+                                        'builderModel': {
+                                            'mark': {
+                                                'color': ui.draggable[0].textContent
+                                            }
+                                        }
+                                    }),
+                                    success: function(data){
+                                        console.log(data);
+                                    }
+                                });
                             }
                         });
 
